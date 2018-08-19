ba3bd56580e0f70593051f153aa151774a0e33f8
ck <305626540@qq.com>
2018-08-19 17:36:05 +0800

    first commit

---
+++ index/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+from . import views as index_views
+from django.conf.urls import url
+urlpatterns = [
+    # path('', views.home, name='home'),
+
+]
---
+++ index/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return render(request,'index.html')
+    # return HttpResponse("xxxxxxxxxxxxxx")
